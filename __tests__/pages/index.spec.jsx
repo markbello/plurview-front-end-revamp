f57c83eff5d62fe9705d9cbccdbf9c88db20524d
markbello <mcbello51286@gmail.com>
2018-11-24 14:17:58 -0500

    Initial Commit: NextJS, Jest/Enzyme, Storybook boilerplate

---
+++ __tests__/pages/index.spec.jsx
@@ -0,0 +1,11 @@
+import {mount} from 'enzyme';
+import IndexPage from '../../pages/index';
+
+describe('Pages', () => {
+  describe('Index', () => {
+    it('should render without throwing an error', function () {
+      const wrap = mount(<IndexPage/>);
+      expect(wrap.find('h1').text()).toBe('My Blog');
+    });
+  });
+});
